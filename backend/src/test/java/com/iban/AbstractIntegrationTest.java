8b1f65cec692e8395bd617b65acdc3bb81f34ec4
Maximilian Dollinger <max@dollinger.dev>
2026-03-22 12:22:22 +0100

    add: moved to SpringBoot 4.0

--- backend/src/test/java/com/iban/AbstractIntegrationTest.java
+++ backend/src/test/java/com/iban/AbstractIntegrationTest.java
@@ -1,8 +1,8 @@
 package com.iban;
 
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
