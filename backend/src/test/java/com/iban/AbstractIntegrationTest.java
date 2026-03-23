8aa00ae5d26b17479939ece9add18e1d36fa0407
Maximilian Dollinger <max@dollinger.dev>
2026-03-23 06:35:58 +0100

    chore: update to testconatiner 2+

--- backend/src/test/java/com/iban/AbstractIntegrationTest.java
+++ backend/src/test/java/com/iban/AbstractIntegrationTest.java
@@ -3,14 +3,14 @@ package com.iban;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 public abstract class AbstractIntegrationTest {
 
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine");
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18-alpine");
 
     static {
         postgres.start();
